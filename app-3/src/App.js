5690f74c87e511bf39ea7d9b01502703d2f220ff
Anders Lofgran <anders.lofgran@gmail.com>
2020-01-29 14:59:42 -0700

    app-3 done

--- app-3/src/App.js
+++ app-3/src/App.js
@@ -2,25 +2,32 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props){
+    super(props)
+
+    this.state = {
+      inputText: '',
+      stuff: ['cake', 'sushi', 'cats', 'programming', 'yeah']
+    }
+  }
+
+  handleInputChange(val){
+    this.setState({inputText: val})
+  }
+  
+  render(){
+    let displayStuff = this.state.stuff
+      .filter(el => el.includes(this.state.inputText))
+      .map(el => <h2>{el}</h2>)
+    
+    return (
+      <div className="App">
+        <input onChange={e => this.handleInputChange(e.target.value)}/>
+        <div>{displayStuff}</div>
+      </div>
+    );
+  }
 }
 
 export default App;
