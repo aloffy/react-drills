31a3ae1f939a3658eb1293cbb5e432e5ec5d0874
Anders Lofgran <anders.lofgran@gmail.com>
2020-01-29 16:15:34 -0700

    app-6 done

--- app-6/src/App.js
+++ app-6/src/App.js
@@ -1,26 +1,40 @@
 import React from 'react';
-import logo from './logo.svg';
+import Todo from './Components/Todo'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props){
+    super(props)
+
+    this.state = {
+      list: [],
+      task: ''
+    }
+  }
+
+  handleInputChange = (val) => {
+    this.setState({ task: val})
+  }
+  addTask = () => {
+    this.setState({
+      list: ([...this.state.list, this.state.task])
+    })
+  }
+
+  render(){
+    let list = this.state.list.map(el => <Todo task={el}/>)
+
+    return (
+      <div className='App'>
+        <h1>My to-do list:</h1>
+        <div>
+          <input onChange={ev => this.handleInputChange(ev.target.value)}/>
+          <button onClick={this.addTask}>Add Task</button>
+        </div>
+        {list}
+      </div>
+    )
+  }
 }
 
 export default App;
